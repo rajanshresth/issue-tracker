cf70f43849e8676085e47ca3eb6e8f403a66b382
rajanshresth <shrestharajan726@gmail.com>
2023-10-19 20:28:04 +0545

    Showing the issue

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -15,3 +15,8 @@ export async function POST(request:NextRequest){
 
     return NextResponse.json(newIssue,{status:201})
 }
+
+export async function GET(request:NextRequest) {
+    const issue=await prisma.issue.findMany();
+    return NextResponse.json(issue);
+}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,46 @@
-import { Button } from '@radix-ui/themes'
+import { Button, Table } from '@radix-ui/themes'
+import axios from 'axios'
 import Link from 'next/link'
 import React from 'react'
 
-const IssuesPage = () => {
+const IssuesPage = async() => {
+  const getData = async() => {
+    const res = await axios.get('http://localhost:3000/api/issues')
+    return res.data;
+  }
+  const issues = await getData();
   return (
     <div>
-      <Button>
-        <Link href='/issues/new'>
-          New Issue
-        </Link>
-      </Button>
+      <div className='mb-5'>
+        <Button>
+          <Link href='/issues/new'>
+            New Issue
+          </Link>
+        </Button>
+      </div>
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue:any) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className='block md:hidden'>{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>{issue.status}</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                {new Date(issue.createdAt).toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   )
 }
