eb04de15a253e78221eb2ec38b37d89bb00d50b5
rajanshresth <shrestharajan726@gmail.com>
2023-10-20 17:59:39 +0545

    Build the edit issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,63 +1,9 @@
-"use client"
-import { Button, Callout, TextField } from '@radix-ui/themes'
-import React, { useState } from 'react'
-import dynamic from 'next/dynamic';
-import {useForm,Controller} from 'react-hook-form';
-import "easymde/dist/easymde.min.css";
-import { useRouter } from 'next/navigation';
-import axios from 'axios';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchema';
-import {z} from 'zod';
-import {ErrorMessage,Spinner} from '@/app/components'
-
-type IssueForm=z.infer<typeof createIssueSchema>
-const SimpleMDE=dynamic(()=>import('react-simplemde-editor'),{ssr:false});
+import React from 'react'
+import IssueForm from '../_components/IssueForm'
 
 const NewIssuePage = () => {
-    const router = useRouter()
-    const {register,control,handleSubmit, formState:{errors}} = useForm<IssueForm>({
-        resolver: zodResolver(createIssueSchema)
-    });
-    const [isSubmitting,setSubmitting]=useState(false);
-    const [error,setError]=useState('');
-    const onSubmit=handleSubmit(async (data:IssueForm) => {
-        try {
-            setSubmitting(true);
-            await axios.post('/api/issues',data);
-            router.push('/issues');
-        } catch (error) {
-            setSubmitting(false);
-            setError('An unexpected error occured.')
-        }
-    })
   return (
-    <div>
-        {error && (
-            <Callout.Root color='red' className='mb-5'>
-                <Callout.Text>
-                    {error}
-                </Callout.Text>
-            </Callout.Root>
-        )}
-        
-    <form className='max-w-xl space-y-5' onSubmit={onSubmit}
-    >
-        <TextField.Root>
-            <TextField.Input placeholder='Title' {...register('title')} />
-        </TextField.Root>
-        <ErrorMessage>{errors.title?.message}</ErrorMessage>
-        <Controller 
-            name="description"
-            control={control}
-            render={({field})=><SimpleMDE placeholder='Description' {...field} />}
-        />
-         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={isSubmitting}>
-            Submit Issue{isSubmitting && <Spinner/>}
-        </Button>
-    </form>
-    </div>
+    <IssueForm />
   )
 }
 
