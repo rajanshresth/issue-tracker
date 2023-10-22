cdf2a87195c42285a11b2788640b7da427db3768
rajanshresth <shrestharajan726@gmail.com>
2023-10-22 22:08:19 +0545

    Fix avatar not loading

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -36,7 +36,8 @@ const NavBar = () => {
               && 
                  <DropdownMenu.Root>
                     <DropdownMenu.Trigger>
-                      <Avatar src={session.user?.image!} fallback="?" size={'2'} radius='full' className='cursor-pointer' />
+                      <Avatar src={session.user?.image!} fallback="?" size={'2'} radius='full' className='cursor-pointer'
+                      referrerPolicy='no-referrer' />
                     </DropdownMenu.Trigger>
                     <DropdownMenu.Content>
                       <DropdownMenu.Label>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "refferrer-policy",
+                        value: "no-referrer",
+                    },
+                ],
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
