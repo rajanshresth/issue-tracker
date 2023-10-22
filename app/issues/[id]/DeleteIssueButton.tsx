13439ab1e519588aaf46debe959ba6e38ed6c2e7
rajanshresth <shrestharajan726@gmail.com>
2023-10-22 11:34:38 +0545

    delete an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,13 @@
 "use client"
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const DeleteIssueButton = ({issueId}:{issueId:number}) => {
+  const router = useRouter();
+  console.log(issueId)
+
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger>
@@ -24,7 +29,11 @@ const DeleteIssueButton = ({issueId}:{issueId:number}) => {
             </Button>
           </AlertDialog.Cancel>
           <AlertDialog.Action>
-            <Button color='red'>
+            <Button color='red' onClick={async () => {
+              await axios.delete('/api/issues/'+issueId);
+              router.push('/issues');
+              router.refresh();
+            }}>
               Delete Issue
             </Button>
           </AlertDialog.Action>
