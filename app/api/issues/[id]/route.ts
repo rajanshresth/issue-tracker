b5ecc864760bc547d325fba39bfdb155c315dea3
rajanshresth <shrestharajan726@gmail.com>
2023-10-22 22:57:17 +0545

    Secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,15 @@
 import { IssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(
     request:NextRequest,
     {params}:{params:{id:string}}){
+        const session = await getServerSession(authOptions);
+        if(!session)
+            return NextResponse.json({},{status:401});
         const body = await request.json();
         const validation=IssueSchema.safeParse(body);
         if(!validation.success){
@@ -29,6 +34,9 @@ export async function PATCH(
 export async function DELETE(
     request:NextRequest,
     {params}:{params:{id:string}}){
+        const session = await getServerSession(authOptions);
+        if(!session)
+            return NextResponse.json({},{status:401});
         const issue=await prisma.issue.findUnique({
             where: {id:parseInt(params.id)}
         });
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,8 +5,11 @@ import React from 'react'
 import EditIssueButton from './EditIssueButton'
 import IssueDetails from '../IssueDetails'
 import DeleteIssueButton from './DeleteIssueButton'
+import { getServerSession } from 'next-auth'
+import authOptions from '@/app/auth/authOptions'
 
 const IssueDetailPage = async({params}:{params:{id:string}}) => {
+    const session=await getServerSession(authOptions)
     const issue = await prisma.issue.findUnique({
     where: {id: parseInt(params.id)}
     })
@@ -18,12 +21,12 @@ const IssueDetailPage = async({params}:{params:{id:string}}) => {
         <Box className='lg:col-span-4'>
           <IssueDetails issue={issue} />
         </Box>
-        <Box>
+        {session && <Box>
           <Flex direction={'column'} gap={'2'}>
             <EditIssueButton issueId={issue.id}/>
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
-        </Box>
+        </Box>}
     </Grid>
   )
 }
