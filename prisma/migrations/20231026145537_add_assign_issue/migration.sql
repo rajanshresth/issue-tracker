0bc24c44d50567a2db8ac19f2b4a529125526326
rajanshresth <shrestharajan726@gmail.com>
2023-10-27 21:27:03 +0545

    implementing api for assigneUser

--- prisma/migrations/20231026145537_add_assign_issue/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Issue" ADD COLUMN     "assignedToUserid" VARCHAR(255);
-
--- AddForeignKey
-ALTER TABLE "Issue" ADD CONSTRAINT "Issue_assignedToUserid_fkey" FOREIGN KEY ("assignedToUserid") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
