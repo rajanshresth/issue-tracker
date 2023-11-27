deea23cb260eccd597093afeabd0609c40447947
rajanshresth <shrestharajan726@gmail.com>
2023-11-27 23:56:42 +0545

    Filter issue

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,17 +1,24 @@
 "use client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Statuses: { label: string; value: string }[] = [
-  { label: "All", value: "all" },
-  { label: "Open", value: "open" },
-  { label: "In Progress", value: "in-progress" },
-  { label: "Closed", value: "closed" },
+  { label: "All", value: "" },
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED " },
 ];
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const query = status ? `?status=${status}` : "";
+        router.push(`/issues${query}`);
+      }}
+    >
       <Select.Trigger placeholder="Filter by status" />
       <Select.Content>
         {Statuses.map((status) => (
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,55 +1,68 @@
-import axios from 'axios'
-import { Table } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
-import IssueStatusBadge from '../components/IssueStatusBadge'
-import IssueActionButton from './IssueActionButton'
-import prisma from '@/prisma/client'
+import { Table } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import IssueActionButton from "./IssueActionButton";
+import prisma from "@/prisma/client";
+import { Status } from "@prisma/client";
 
+interface Props {
+  searchParams: { status: Status };
+}
 
-const IssuesPage = async() => {
-  // const getData = async() => {
-  //   const res = await fetch('/api/issues');
-  //   return res.json();
-  // }
-  // const issues = await getData();
-  const issues = await prisma.issue.findMany();
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
   return (
     <div>
       <IssueActionButton />
-      <Table.Root variant='surface'>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
-        {issues.map((issues:any) => (
-            <Table.Body key={issues.id}>
-              <Table.Row key={issues.id}>
-                <Table.Cell>
-                  <Link href={`/issues/${issues.id}`} className='font-semibold text-blue-600 dark:text-blue-500 hover:underline'>
-                    {issues.title}
-                  </Link>
-                  <div className='block md:hidden'>
-                    <IssueStatusBadge status={issues.status} />
-                  </div>
-                </Table.Cell>
-                <Table.Cell className='hidden md:table-cell'>
+        {issues.map((issues: any) => (
+          <Table.Body key={issues.id}>
+            <Table.Row key={issues.id}>
+              <Table.Cell>
+                <Link
+                  href={`/issues/${issues.id}`}
+                  className="font-semibold text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  {issues.title}
+                </Link>
+                <div className="block md:hidden">
                   <IssueStatusBadge status={issues.status} />
-                </Table.Cell>
-                <Table.Cell className='hidden md:table-cell'>
-                  {new Date(issues.createdAt).toDateString()}
-                </Table.Cell>
-             </Table.Row>
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issues.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {new Date(issues.createdAt).toDateString()}
+              </Table.Cell>
+            </Table.Row>
           </Table.Body>
         ))}
       </Table.Root>
     </div>
-  )
-}
+  );
+};
 
-export const dynamic='force-dynamic';
+export const dynamic = "force-dynamic";
 
-export default IssuesPage
+export default IssuesPage;
