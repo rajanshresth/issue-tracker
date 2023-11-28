68bc8ab951d591c565bcc26d71341d585cb00006
rajanshresth <shrestharajan726@gmail.com>
2023-11-28 22:59:43 +0545

    Implementing pagination

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,16 @@ import IssueChart from './IssueChart';
 import { Flex } from '@radix-ui/themes';
 import Pagination from "./components/Pagination";
 
-export default async function Home() {
-  const open = await prisma.issue.count({where:{status:'OPEN'}});
-  const inProgress = await prisma.issue.count({where:{status:'IN_PROGRESS'}});
-  const closed = await prisma.issue.count({where:{status:'CLOSED'}});
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
     <main>
       <Flex direction={"column"} gap={"5"}>
@@ -18,7 +24,11 @@ export default async function Home() {
         <span className="border-b-4 border-indigo-500"></span>
         <h1 className="text-blue-700 font-bold text-3xl">Latest Issue: </h1>
         <LatestIssues />
-        <Pagination itemCount={100} pageSize={10} currentPage={1} />
+        <Pagination
+          itemCount={100}
+          pageSize={10}
+          currentPage={parseInt(searchParams.page)}
+        />
       </Flex>
     </main>
   );
